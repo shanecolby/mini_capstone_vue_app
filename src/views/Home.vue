e1bc709cff66ad24fbe2386c7b7836ab4959ffdb
shane <shanecolbyzero8@gmail.com>
2020-12-09 21:28:45 -0600

    make index page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,22 +1,40 @@
 <template>
   <div class="home">
     <h1>{{ message }}</h1>
+    <div v-for="product in Products">
+      {{ product.name }}
+      <hr>
+    </div>
   </div>
 </template>
 <style>
 </style>
 <script>
+import axios from 'axios';
 export default {
   data: function () {
     return {
       message: "Welcome to Vue.js!",
-      products: [
+      Products: [
         { id: 1, name: "Product 1", price: 10 },
         { id: 2, name: "Product 2", price: 230 },
-      ],
+      ]
+      products: []
     };
   },
-  created: function () {},
-  methods: {},
+  created: function () {
+    console.log('in created');
+    this.productsIndex();
+  },
+  methods: {
+    productsIndex: function(){
+      console.log('products index..');
+      axios.get('/api/products').then(response => {
+        console.log(response.data);
+        this.products = response.data;
+      });
+    }
+
+  },
 };
 </script>
